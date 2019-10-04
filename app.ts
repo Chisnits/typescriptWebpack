2a7df9df4d507c798858c879838b5d8818156b7b
Chisnits <chasederr@gmail.com>
2019-10-03 22:35:28 -0600

    data types in typescript

--- app.ts
+++ app.ts
@@ -1,16 +1,21 @@
-export class Hero {
-    id: number;
-    name: string;
+// Implicit data types. Cannot reassign to a different data type.
+//string
+// let myName = 'Chase';
+// myName = 29;
 
-    constructor(name) {
-        this.name = name;
-    }
+//number
+// let myAge = 25.5;
+// myAge = 'Chase';
 
-    myName()
-    {
-        return this.name;
-    }
-  }
+//boolean
+// let hasHobbies = true;
+// hasHobbies = 1;
 
-  let hero = new Hero('Chase');
-  console.log(hero.myName());
+//assign types
+let myRealAge: number;
+myRealAge = 25;
+// myRealAge = "25";
+
+//array
+let hobbies = ["Cooking", "Sports"];
+console.log(hobbies[0]);
