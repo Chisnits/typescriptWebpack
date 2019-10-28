538d41c25729718ecf5b94d77dfeb487fdf2bcf2
Chisnits <chasederr@gmail.com>
2019-10-27 20:34:23 -0600

    section 5 complete

--- app.ts
+++ app.ts
@@ -243,3 +243,45 @@ class Helpers {
 }
 console.log(2 * Helpers.PI);
 console.log(Helpers.calcCircumference(8));
+
+//Abstract Classes
+abstract class Project {
+    projectName: string = "Default";
+    budget: number = 1000;
+
+    abstract changeName(name: string): void;
+    
+    calcBudget() {
+        return this.budget * 2;
+    }
+}
+
+class ITProject extends Project {
+    changeName(name: string): void {
+        this.projectName = name;
+    }
+}
+
+let newProject = new ITProject();
+console.log(newProject);
+newProject.changeName("Super IT Project");
+console.log(newProject);
+
+//Private Constructors
+class OnlyOne {
+    private static instance: OnlyOne;
+
+    private constructor(public readonly name: string) {}
+
+    static getInstance() {
+        if (!OnlyOne.instance) {
+            OnlyOne.instance = new OnlyOne('The Only One');
+        }
+        return OnlyOne.instance;
+    }
+}
+
+// let wrong = new OnlyOne('The Only One');
+let right = OnlyOne.getInstance();
+console.log(right.name);
+// right.name = 'Something Else';
